79f2e813bbc15d51d098d93cd442ce7540ea9f37
Aleksas Murauskas <aleksas.murauskas@mail.mcgill.ca>
2019-02-20 17:41:15 -0500

    Fibboncci pseudocode taken from online

--- G11_Lab 2/Fibonnacci.s
+++ G11_Lab 2/Fibonnacci.s
@@ -1,3 +1,29 @@
 
 
-	
+//taken from link      https://rosettacode.org/wiki/Fibonacci_sequence#8080_Assembly
+	.text
+	.global _start
+
+
+_start:
+	push{r1-r3}
+	ldr r1, #0 
+	ldr r2, #1
+
+FIB: 
+	mov r3, r2
+	add r2, r1, r2
+	mov r1, r3
+	sub r0, r0, #1
+	cmp r0, #1
+	bne  fibloop 
+
+	mov r0, r2
+	pop {r1-r3}
+	mov pc, lr 
+
+
+
+RESULT: .word 0 
+
+N: .word 4
